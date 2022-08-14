1767473a31910be0b234751382e43cf9af68aa29
MaryBlessing <marybngozi@gmail.com>
2022-08-14 09:55:17 +0000

    fix int print

--- print_int.c
+++ print_int.c
@@ -1,4 +1,48 @@
+#include <stdio.h>
 #include "main.h"
+
+/**
+ * count_int - prints integers
+ * @n: input number
+ * Return: number of chatacters
+ */
+
+int count_int(int n)
+{
+	int x = 0;
+
+	if (n < 0)
+	{
+		x++;
+		n = -n;
+	}
+	while (n / 10)
+	{
+		n = n / 10;
+		x++;
+	}
+	return (x);
+}
+
+
+/**
+ * print_num - prints integers
+ * @n: input number
+ * Return: number of chatacters printed
+ */
+
+void print_num(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_num(n / 10);
+	_putchar(n % 10 + '0');
+}
+
 /**
  * print_int - prints integers
  * @n: input number
@@ -7,19 +51,7 @@
 
 int print_int(int n)
 {
-	int y = 0;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		y++;
-		n = -n;
-	}
-	while (n / 10)
-	{
-		_putchar(n % 10 + '0');
-		n = n / 10;
-		y++;
-	}
-	return (y);
+	print_num(n);
+	return (count_int(n));
 }
+
