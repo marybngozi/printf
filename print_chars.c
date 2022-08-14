def0e7267bed849c5d31186fa92418d1f43a54c2
MaryBlessing <marybngozi@gmail.com>
2022-08-14 10:16:34 +0000

    fix print_chars

--- print_chars.c
+++ print_chars.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _print_chars - prints characters using putchar
  * @str: characters to print
@@ -10,7 +11,7 @@ int _print_chars(char *str)
 
 	if (!str)
 	{
-		return (j);
+		str = "(null)";
 	}
 	while (str[j])
 	{
